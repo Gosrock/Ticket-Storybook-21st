8456cbd0d933f5e11ff31ce8c155460546aa8c60
이찬진 <water0641@naver.com>
2022-01-24 15:46:39 +0900

    refactor(pagetest): 디렉토리 이동 pageTest

--- src/stories/Example/PageTest/Page.jsx
+++ src/stories/Example/PageTest/Page.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import "./page.css";
-import { Header } from "../Header/Header.jsx";
+import './page.css';
+import { Header } from '../Header/Header.jsx';
 
 export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <article>
@@ -16,14 +16,14 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
     <section>
       <h2>Pages in Storybook</h2>
       <p>
-        We recommend building UIs with a{" "}
+        We recommend building UIs with a{' '}
         <a
           href="https://componentdriven.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           <strong>component-driven</strong>
-        </a>{" "}
+        </a>{' '}
         process starting with atomic components and ending with pages.
       </p>
       <p>
@@ -42,7 +42,7 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
         </li>
       </ul>
       <p>
-        Get a guided tutorial on component-driven development at{" "}
+        Get a guided tutorial on component-driven development at{' '}
         <a
           href="https://storybook.js.org/tutorials/"
           target="_blank"
@@ -50,7 +50,7 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
         >
           Storybook tutorials
         </a>
-        . Read more in the{" "}
+        . Read more in the{' '}
         <a
           href="https://storybook.js.org/docs"
           target="_blank"
@@ -61,7 +61,7 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
         .
       </p>
       <div className="tip-wrapper">
-        <span className="tip">Tip</span> Adjust the width of the canvas with the{" "}
+        <span className="tip">Tip</span> Adjust the width of the canvas with the{' '}
         <svg
           width="10"
           height="10"
@@ -85,9 +85,9 @@ Page.propTypes = {
   user: PropTypes.shape({}),
   onLogin: PropTypes.func.isRequired,
   onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
+  onCreateAccount: PropTypes.func.isRequired
 };
 
 Page.defaultProps = {
-  user: null,
+  user: null
 };
