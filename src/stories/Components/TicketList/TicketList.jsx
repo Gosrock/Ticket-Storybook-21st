ee815252ecc4b193d052159d84dd3dbb49fbb812
yi219 <tbc07061@naver.com>
2022-01-31 19:05:05 +0900

    test(test): ticketlist
    ticketlist

---
+++ src/stories/Components/TicketList/TicketList.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TicketList.css';
+import { ReactComponent as GosrockLogo } from './GosrockLogo.svg';
+import {
+  입금확인중,
+  입금확인,
+  미입금처리,
+  입장완료
+} from '../StateIcon/StateIcon.stories.jsx';
+
+export const TicketList = ({
+  date1,
+  date2,
+  performdate,
+  bookdate,
+  ...props
+}) => {
+  return (
+    <>
+      <button className={['TicketList'].join(' ')} {...props}>
+        <div className={'TicketList-ticketState'}>
+          <입금확인></입금확인>
+        </div>
+        <div className={'TicketList-rec'}></div>
+        <div className={'TicketList-info'}>
+          <div>
+            <p>
+              <span style={{ fontWeight: '700' }}>공연날짜</span> {performdate}
+            </p>
+          </div>
+          <div>
+            <p>
+              <span style={{ fontWeight: '700' }}>예매일시</span> {bookdate}
+            </p>
+          </div>
+        </div>
+
+        <GosrockLogo className={'TicketList-icon'} />
+      </button>
+    </>
+  );
+};
+
+TicketList.propTypes = {
+  date1: PropTypes.string.isRequired,
+  date2: PropTypes.string.isRequired,
+  performdate: PropTypes.string.isRequired,
+  bookdate: PropTypes.string.isRequired,
+  onClick: PropTypes.func
+};
+
+TicketList.defaultProps = {
+  onClick: undefined,
+  date1: '관람일시',
+  date2: '예매일시',
+  performdate: '관람일',
+  bookdate: '공연일'
+};
